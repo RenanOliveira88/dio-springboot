0b8731f91f8366be37e518a415b20fa47e76af3e
Renan Oliveira <c.renan.o@gmail.com>
2025-10-08 21:53:52 -0300

    feat: SpringBoot project initialization

---
+++ demospringboot/src/test/java/edu/dio/springboot/exercises/demospringboot/DemospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dio.springboot.exercises.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
